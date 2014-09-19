f42fe0d7b2317f456fe0ce4de983cefb32a516d9
deadean <deadean@yandex.ru>
2014-09-19 15:51:36 +0300

    #123

--- Sample/src/MainApplicationSample/Program.cs
+++ Sample/src/MainApplicationSample/Program.cs
@@ -13,7 +13,7 @@ namespace Blank.ConsoleMain
                 var mc = new ModelServices();
                 var companies = mc.GetAllUsers();
 
-                companies.ForEach(x => Console.WriteLine(x.Name));
+                companies.ForEach(x => Console.WriteLine(x.Username));
             }
             catch (Exception ex)
             {
